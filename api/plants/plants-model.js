f3de88ad5c3cbeebb1f7c6e824d2f7d8a54bf1e8
Jonathan1600 <jonathancalderonsilberman@gmail.com>
2021-04-26 16:55:47 -0700

    add working now

--- api/plants/plants-model.js
+++ api/plants/plants-model.js
@@ -11,8 +11,9 @@ const findById = (plant_id) => {
 }
 
 const add = async (newPlant) => {
-    const [plant_id] = await db('plants').insert(newPlant)
-    return findById(plant_id);
+    const [plant_id] = await db('plants').insert(newPlant, "plant_id");
+    console.log(plant_id)
+    return findById(plant_id)
 }
 
 const update = async (id, updatedPlant) => {
