fd3d38d9d3b10a929a1d810f2fb548fd669fce8a
Jonathan1600 <jonathancalderonsilberman@gmail.com>
2021-04-26 13:56:19 -0700

    getById done

--- api/plants/plants-model.js
+++ api/plants/plants-model.js
@@ -4,6 +4,14 @@ function find() {
     return db('plants')
 }
 
+const findById = (plant_id) => {
+    return db('plants')
+        .where("plant_id", plant_id)
+        .first()
+}
+
+
 module.exports = {
     find,
+    findById,
 }
--- api/plants/plants-router.js
+++ api/plants/plants-router.js
@@ -1,18 +1,21 @@
 const router = require('express').Router();
 const Plants = require('./plants-model');
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
     Plants.find()
         .then(plants => {
             res.status(200).json(plants)
         })
-        .catch(err => {
-            res.status(500).json({ message: err.message })
-        })
+        .catch(next)
 })
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', (req, res, next) => {
+    const { id } = req.params;
+    Plants.findById(id)
+        .then(plant => {
+            res.status(200).json(plant);
+        })
+        .catch(next)
 })
 
 router.post('/', (req, res) => {
@@ -25,6 +28,15 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
 
+})
+
+router.use((err, req, res, next) => {// eslint-disable-line
+    res.status(req.status || 500).json({
+        message: "Something went terribly wrong.",
+        error: err.message,
+        stack: err.stack,
+        err: err
+    })
 })
 
 module.exports = router;
