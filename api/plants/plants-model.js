948b0d71985a276148774ea71f9bf80f4805c1d7
Jonathan1600 <jonathancalderonsilberman@gmail.com>
2021-04-26 14:59:38 -0700

    update and add done

--- api/plants/plants-model.js
+++ api/plants/plants-model.js
@@ -10,8 +10,20 @@ const findById = (plant_id) => {
         .first()
 }
 
+const add = async (newPlant) => {
+    const [plant_id] = await db('plants').insert(newPlant);
+    return findById(plant_id);
+}
+
+const update = async (id, updatedPlant) => {
+    await db('plants').update(updatedPlant).where("plant_id", id);
+    return findById(id);
+}
+
 
 module.exports = {
     find,
     findById,
+    add,
+    update
 }
