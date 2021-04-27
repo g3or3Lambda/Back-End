85ac432f3ecc2f2e22589cc0b991cdd41255d22b
Randy Ortiz <Randyortiz28@gmail.com>
2021-04-27 12:11:37 -0400

    added restriction

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,17 +1,18 @@
 //authenticated users must be able to update their own phone number and password
-const router = require('express').Router();
-const Users = require('./users-model');
+const router = require('express').Router()
+const Users = require('./users-model')
+const restricted = require('../auth/auth-middleware')
 
-router.put('/:id', (req, res) => {
-    const { id } = req.params
-    const changes = req.body;
-    Users.update(id, changes)
-        .then(updated => {
-            res.status(200).json(updated)
-        })
-        .catch(err => {
-            res.status(500).json({ message: err. message })
-        })
+router.put('/:id', restricted, (req, res) => {
+  const { id } = req.params
+  const changes = req.body
+  Users.update(id, changes)
+    .then((updated) => {
+      res.status(200).json(updated)
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message })
+    })
 })
 
-module.exports = router;
+module.exports = router
